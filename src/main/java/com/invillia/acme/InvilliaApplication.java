1f118c2050b48c824efae19dc6ec13c3596877fd
Andre Cintra <andrefcintra@hotmail.com>
2019-01-19 15:36:55 -0200

    Modified application controller to call our lambda handler

--- src/main/java/com/invillia/acme/InvilliaApplication.java
+++ src/main/java/com/invillia/acme/InvilliaApplication.java
@@ -2,11 +2,37 @@ package com.invillia.acme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import com.invillia.acme.controller.StoreController;
 
 @SpringBootApplication
-public class InvilliaApplication {
+@Import({StoreController.class})
+public class InvilliaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvilliaApplication.class, args);
-	}
+  /*
+   * Create required HandlerMapping, to avoid several default HandlerMapping instances being created
+   */
+  @Bean
+  public HandlerMapping handlerMapping() {
+    return new RequestMappingHandlerMapping();
+  }
+
+  /*
+   * Create required HandlerAdapter, to avoid several default HandlerAdapter instances being created
+   */
+  @Bean
+  public HandlerAdapter handlerAdapter() {
+    return new RequestMappingHandlerAdapter();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(InvilliaApplication.class, args);
+  }
 }
